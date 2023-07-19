35b29bb95018f5ae08f5317ed3d9a7414251e370
“Dami-LEE00” <ecch74@naver.com>
2023-07-19 17:29:47 +0900

    update netflix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
 
 const Login = () => {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
   return (
     <div className='Login'>
       <div className='login-bg'>
@@ -14,12 +17,18 @@ const Login = () => {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>아이디</Form.Label>
-              <Form.Control type="text" placeholder="아이디를 입력해주세요" />
+              <Form.Control 
+                type="text" 
+                placeholder="아이디를 입력해주세요" 
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>비밀번호</Form.Label>
-              <Form.Control type="password" placeholder="비밀번호를 입력해주세요" />
+              <Form.Control 
+                type="password" 
+                placeholder="비밀번호를 입력해주세요" 
+              />
             </Form.Group>
             <Button variant="danger" type="submit">
               로그인
